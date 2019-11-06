762066c904bd072c707b6d302eb2cce645e40d31
“Alice <alicexenawong@gmail.com>
2019-11-05 18:22:40 -0800

    Add random product to database. Note: random products are coffee.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,4 +5,29 @@ class Api::ProductsController < ApplicationController
     render 'products.json.jb'
   end
 
+  def pirate_ship
+    @pirate_ship = Product.first
+    render 'pirate_ship.json.jb'
+  end
+
+  def dragon_egg
+    @dragon_egg = Product.second
+    render 'dragon_egg.json.jb'
+  end
+
+  def pandoras_box
+    @pandoras_box = Product.third
+    render 'pandoras_box.json.jb'
+  end
+
+  def killer_robot
+    @killer_robot = Product.fourth
+    render 'killer_robot.json.jb'
+  end
+
+  def balloon
+    @balloon = Product.fifth
+    render 'balloon.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/all_products" => "products#all_products"
+    get "/pirate_ship" => "products#pirate_ship"
+    get "/dragon_egg" => "products#dragon_egg"
+    get "/pandoras_box" => "products#pandoras_box"
+    get "/killer_robot" => "products#killer_robot"
+    get "/balloon" => "products#balloon"
   end
 end
