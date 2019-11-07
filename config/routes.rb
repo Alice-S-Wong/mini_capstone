0b70b9e8a5817754c6153ffe9b8b018012d60609
“Alice <alicexenawong@gmail.com>
2019-11-06 18:54:20 -0800

    Add functionality to display all products with a single route using parameters.

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/all_products" => "products#all_products"
-    get "/pirate_ship" => "products#pirate_ship"
-    get "/dragon_egg" => "products#dragon_egg"
-    get "/pandoras_box" => "products#pandoras_box"
-    get "/killer_robot" => "products#killer_robot"
-    get "/balloon" => "products#balloon"
+    get "/products" => "products#one_product"
+    get "/products/:name" => "products#one_product"
+    # get "/pirate_ship" => "products#pirate_ship"
+    # get "/dragon_egg" => "products#dragon_egg"
+    # get "/pandoras_box" => "products#pandoras_box"
+    # get "/killer_robot" => "products#killer_robot"
+    # get "/balloon" => "products#balloon"
   end
 end
