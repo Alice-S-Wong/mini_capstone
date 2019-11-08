0b70208e6664c05c991d61fbb4edaad79b75ca1a
“Alice <alicexenawong@gmail.com>
2019-11-07 18:13:29 -0800

    Add RESTful show method to project.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,7 +2,13 @@ class Api::ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    render 'products.json.jb'
+    render 'index.json.jb'
+  end
+
+  def show
+    user_id = params[:id]
+    @product = Product.find_by(id: user_id)
+    render 'show.json.jb'
   end
 
 end
