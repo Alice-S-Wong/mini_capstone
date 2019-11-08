06447c7113b88714d52c3e4a55b3a579a42b3182
“Alice <alicexenawong@gmail.com>
2019-11-07 17:43:32 -0800

    Clean up file and delete outdated files/code.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,34 +5,4 @@ class Api::ProductsController < ApplicationController
     render 'products.json.jb'
   end
 
-  # def one_product
-  #   @one_product = Product.find_by(name: params[:name])
-  #   render 'one_product.json.jb'
-  # end
-
-  # def pirate_ship
-  #   @pirate_ship = Product.first
-  #   render 'pirate_ship.json.jb'
-  # end
-
-  # def dragon_egg
-  #   @dragon_egg = Product.second
-  #   render 'dragon_egg.json.jb'
-  # end
-
-  # def pandoras_box
-  #   @pandoras_box = Product.third
-  #   render 'pandoras_box.json.jb'
-  # end
-
-  # def killer_robot
-  #   @killer_robot = Product.fourth
-  #   render 'killer_robot.json.jb'
-  # end
-
-  # def balloon
-  #   @balloon = Product.fifth
-  #   render 'balloon.json.jb'
-  # end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,5 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/products" => "products#index"
-    # get "/products" => "products#one_product"
-    # get "/products/:name" => "products#one_product"
-    # post "/body_param" => "products#body"
-    # get "/pirate_ship" => "products#pirate_ship"
-    # get "/dragon_egg" => "products#dragon_egg"
-    # get "/pandoras_box" => "products#pandoras_box"
-    # get "/killer_robot" => "products#killer_robot"
-    # get "/balloon" => "products#balloon"
   end
 end
