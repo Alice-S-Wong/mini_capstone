69813975cfe98eaa873a5e3aef1adeb1176665f3
Alice <alicexenawong@gmail.com>
2019-11-19 18:41:49 -0800

    Add order functionality to app. Add show and index actions. Add create order action.

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,34 @@
+class Api::OrdersController < ApplicationController
+  def create
+    tax_rate = 0.09
+    if current_user
+      subtotal = Product.find_by(id: params[:product_id]).price * params[:quantity].to_i
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: subtotal,
+        tax: subtotal * tax_rate,
+        total: subtotal * (1 + tax_rate)
+        )
+      @order.save
+      render 'show.json.jb'
+    else
+      render json: {message: "You must log in first"}, status: :unauthorized
+    end
+  end
+
+  def index
+    @orders = current_user.orders
+    render 'index.json.jb'
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    if current_user.id == @order.user_id
+      render 'show.json.jb'
+    else
+      render json: {message: 'User unauthorized to access order'}, status: :unauthorized
+    end
+  end
+end
