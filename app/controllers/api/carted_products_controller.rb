01a56fc4c04a3d74a0b235e282b44acfcbf15d3f
Alice <alicexenawong@gmail.com>
2019-11-24 10:18:41 -0800

    Allow user to view shopping cart. Allow user to order from shopping cart and update carted products accordingly.

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,4 +1,14 @@
 class Api::CartedProductsController < ApplicationController
+  def index
+    if current_user
+      @cart = CartedProduct.where(status: "carted")
+      @cart = @cart.where(user_id: current_user.id)
+      render "index.json.jb"
+    else
+      render json: {message: "You must log in to add items to your cart"}, status: :unauthorized
+    end
+  end
+
   def create
     if current_user
       @carted_product = CartedProduct.new(
--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -18,16 +18,27 @@ class Api::OrdersController < ApplicationController
 
   def create
     tax_rate = 0.09
-    subtotal = Product.find_by(id: params[:product_id]).price * params[:quantity].to_i
+    subtotal = 0
+    cart = CartedProduct.where(status: "carted")
+    cart = cart.where(user_id: current_user.id)
+    cart.each do |carted_product|
+      subtotal += carted_product.quantity * carted_product.product.price
+    end
+    tax = subtotal * tax_rate
+    total = tax + subtotal
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: subtotal,
-      tax: subtotal * tax_rate,
-      total: subtotal * (1 + tax_rate)
+      tax: tax,
+      total: total
     )
     @order.save
+    cart.each do |carted_product|
+      purchased_product = carted_product
+      purchased_product.status = "purchased"
+      purchased_product.order_id = @order.id
+      purchased_product.save
+    end
     render 'show.json.jb'
   end
 end
