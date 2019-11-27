588705062bcc2a7baee11b2b4e9172d83956bbea
Alice <alicexenawong@gmail.com>
2019-11-26 18:13:24 -0800

    Add full-stack show action for products.

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,5 @@ Rails.application.routes.draw do
     post "/carted_products" => "carted_products#create"
     delete "/carted_products/:id" => "carted_products#destroy"
   end
+  get "/products/:id" => "products#show"
 end
