9d77d237551867d446c9c0ffbf914c022ac65067
Alice <alicexenawong@gmail.com>
2019-11-27 17:32:48 -0800

    Add full-stack edit and update actions to products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,37 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "index.html.erb"
   end
+  def new
+    render "new.html.erb"
+  end
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      in_stock: params[:in_stock],
+      supplier_id: params[:supplier_id]
+    )
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
   def show
     @product = Product.find(params[:id])
     render "show.html.erb"
+  end
+  def edit
+    @suppliers = Supplier.all
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.in_stock = params[:in_stock]
+    @product.supplier_id = params[:supplier_id]
+    @product.save
+    redirect_to "/products/#{@product.id}"
   end
 end
